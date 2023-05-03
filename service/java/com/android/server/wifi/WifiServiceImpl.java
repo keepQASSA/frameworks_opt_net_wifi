GrapheneOS/platform_frameworks_opt_net_wifi

Revert "Revert "[DO NOT MERGE] wifi: remove certificates for network factory reset""
Bug: 231985227
This reverts commit d8275ec2b6a32c05766baddc4d7d10c365ad47e1.

Reason for revert: re-release security patch

Change-Id: I91ec19ee9f70a676de96b86c212416f171e15943

service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -3444,7 +3444,14 @@ public class WifiServiceImpl extends BaseWifiService {
         List<WifiConfiguration> networks = mWifiThreadRunner.call(
                 () -> mWifiConfigManager.getSavedNetworks(Process.WIFI_UID),
                 Collections.emptyList());
+        EventLog.writeEvent(0x534e4554, "231985227", -1,
+                "Remove certs for factory reset");
         for (WifiConfiguration network : networks) {
+            if (network.isEnterprise()) {
+                mWifiThreadRunner.run(() ->
+                        mWifiInjector.getWifiKeyStore()
+                                .removeKeys(network.enterpriseConfig, true));
+            }
             removeNetwork(network.networkId, packageName);
         }
         // Delete all Passpoint configurations

keepQASSA/frameworks_opt_net_wifi

(cherry picked from commit 625419f9f60591e304f0dba177df2ba8c998c949)
Merged-In: I91ec19ee9f70a676de96b86c212416f171e15943

service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -3002,7 +3002,13 @@ public class WifiServiceImpl extends BaseWifiService {
                 List<WifiConfiguration> networks = mClientModeImpl.syncGetConfiguredNetworks(
                         Binder.getCallingUid(), mClientModeImplChannel, Process.WIFI_UID);
                 if (networks != null) {
+                        EventLog.writeEvent(0x534e4554, "231985227", -1,
+                            "Remove certs for factory reset");
                     for (WifiConfiguration config : networks) {
+                        if (config.isEnterprise()) {
+                            mWifiInjector.getWifiKeyStore().removeKeys(
+                                    config.enterpriseConfig, true);
+                        }
                         removeNetwork(config.networkId, packageName);
                     }
                 }